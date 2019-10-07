c1573f4cf37aaf8075cdae7eb5fe2ccf870a0949
swati_fulzele <swati_fulzele@persistent.com>
2019-10-07 17:03:26 +0530

    Added param for initial_pose and done related changes

--- launch/turtlebot3_navigation.launch.py
+++ launch/turtlebot3_navigation.launch.py
@@ -17,43 +17,54 @@
 # /* Author: Darby Lim */
 
 import os
+import shutil
 from ament_index_python.packages import get_package_share_directory
 import launch
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
 MAP_PATH = os.environ['HOME']
 
+# Below changes (line no 33 to 46 and 52) are added as a workaround for long path for map file.
+# This change is required for testing on Robomaker.
+pkg_dir = get_package_share_directory('aws_robomaker_small_house_world')
+src_path = os.path.join(pkg_dir, 'maps', 'turtlebot3_waffle_pi')
+
+print("Source path = ", src_path)
+
+directory = '/tmp/maps'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+src_files = os.listdir(src_path)
+for file_name in src_files:
+    full_file_name = os.path.join(src_path, file_name)
+    if os.path.isfile(full_file_name):
+        shutil.copy(full_file_name, directory)
+
 def generate_launch_description():
     use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='false')
-    print('path', os.path.join(
-                      MAP_PATH,
-                      'map.yaml'))
-    map_path = os.path.join(
-                    os.environ['HOME'],
-                    'map.yaml')
-    map_dir = LaunchConfiguration(
-                    'map',
-                    default=map_path)
+
+    map_path = os.path.join('/tmp/maps/'
+                            'map.yaml')
+    map_dir = LaunchConfiguration('map',
+                                  default=map_path)
     param_file_name = TURTLEBOT3_MODEL + '.yaml'
     param_dir = LaunchConfiguration(
         'params',
         default=os.path.join(
-            get_package_share_directory('person_detection_simulation'),
+            get_package_share_directory('aws_robomaker_small_house_world'),
             'param',
-            	param_file_name))
+            param_file_name))
     nav_launch_dir = get_package_share_directory('turtlebot3_navigation2')
     navigation2_node = launch.actions.IncludeLaunchDescription(
         launch.launch_description_sources.PythonLaunchDescriptionSource(
             os.path.join(nav_launch_dir, 'launch', 'navigation2.launch.py')
         ),
-        launch_arguments={'map': map_dir,'use_sim_time': use_sim_time,'params': param_dir}.items()
+        launch_arguments={'map': map_dir, 'use_sim_time': use_sim_time, 'params': param_dir}.items()
     )
 
     return LaunchDescription([
         navigation2_node
     ])
-
